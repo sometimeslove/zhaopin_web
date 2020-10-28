e6443f5238dc7421c2418ba92bd6d8f7319a3a4f
sometimeslove <857508399@qq.com>
2020-10-28 22:22:11 +0800

    提交网站雏形

---
+++ article/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_view
+
+from . import views
+
+app_name="accounts"
+
+urlpatterns = [
+    url(r'^login/$', views.LoginView.as_view(success_url='/'), name='login', kwargs={'authentication_form': LoginForm}),
+    url(r'^register/$', views.RegisterView.as_view(success_url="/"), name='register'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout')
+]
