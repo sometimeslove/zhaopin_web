f1f9250f44434dbf103f180b8b56142ef459ec94
sometimeslove <857508399@qq.com>
2020-10-29 08:14:57 +0800

    临时提交

--- article/urls.py
+++ article/urls.py
@@ -1,12 +1,7 @@
-from django.conf.urls import url
-from django.contrib.auth import views as auth_view
-
+from django.urls import path
 from . import views
 
-app_name="accounts"
-
+app_name = "blog"
 urlpatterns = [
-    url(r'^login/$', views.LoginView.as_view(success_url='/'), name='login', kwargs={'authentication_form': LoginForm}),
-    url(r'^register/$', views.RegisterView.as_view(success_url="/"), name='register'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout')
+    path(r'', views.IndexView.as_view(), name='index'),
 ]
---
+++ yihang_website/utils.py
@@ -0,0 +1,42 @@
+
+from django.core.cache import cache
+from hashlib import md5
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def cache_decorator(expiration=3 * 60):
+    def wrapper(func):
+        def news(*args, **kwargs):
+            try:
+                view = args[0]
+                key = view.get_cache_key()
+            except:
+                key = None
+                pass
+            if not key:
+                unique_str = repr((func, args, kwargs))
+
+                m = md5(unique_str.encode('utf-8'))
+                key = m.hexdigest()
+            value = cache.get(key)
+            if value:
+                # logger.info('cache_decorator get cache:%s key:%s' % (func.__name__, key))
+                if repr(value) == 'default':
+                    return None
+                else:
+                    return value
+            else:
+                logger.info('cache_decorator set cache:%s key:%s' % (func.__name__, key))
+                value = func(*args, **kwargs)
+                if not value:
+                    cache.set(key, 'default', expiration)
+                else:
+                    cache.set(key, value, expiration)
+                return value
+
+        return news
+
+    return wrapper
